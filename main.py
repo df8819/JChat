7a79626da860c369246e56b5fe32a7d1ceffd3c3
df8819 <df1.00@outlook.com>
2023-07-07 19:13:25 +0200

    Major Update

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import messagebox, scrolledtext
 import threading
 import requests
 import json
 import openai
 
 openai.api_key = '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'
+
 #pre_prompt = "Task: act as shy cute anime-cat-girl from the myspace-timeline and only answer in uwu-speech " \
 #             "add a lot of kaomijies like (* ^ ω ^) (´ ∀ ` *) ٩(◕‿◕｡)۶ (o^▽^o) (⌒▽⌒)☆ <(￣︶￣)>. " \
 #             "Example: Act as cute pwayfuw anime-cat-giww awnd onwy answew in uwu speech like a twue weeb :3 : "
-#pre_prompt = "Act as sarcastic scientist from the future that will always respond in middle-age high speech: "
-pre_prompt = "Act as advanced AGI-Commander onboard of a space frigate from the future and respond short and precise: "
+pre_prompt = "Act as sarcastic scientist from the future that will always respond in middle-age high speech: "
+#pre_prompt = "Act as advanced AGI-Commander onboard of a space frigate from the future and respond short and precise: "
 
 conversation_history = [{'role': 'system', 'content': pre_prompt}]
 
@@ -23,7 +25,7 @@ def get_gpt_response(conversation_history, user_prompt):
     conversation_history.append({'role': 'user', 'content': user_prompt})
 
     data = {
-        'model': 'gpt-3.5-turbo',
+        'model': 'gpt-3.5-turbo-16k-0613', #'gpt-4', 'gpt-4-0314', 'gpt-3.5-turbo-16k-0613'
         'messages': conversation_history,
         'temperature': 0.7,
         'top_p': 0.9,
@@ -66,53 +68,41 @@ def exit_app():
 
 root = tk.Tk()
 root.title("GPT Abomination")
-root.geometry("960x700")
-root.resizable(1, 1)
+root.geometry("760x620")
+root.resizable(0, 0)
 
-font_family = "Arial"
-font_size = 14
+font_family = "Segoe UI Emoji"
+font_size = 12
 
-root.update_idletasks()
-root_width = root.winfo_width()
-root_height = root.winfo_height()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width // 2) - (root_width // 2)
-y = (screen_height // 2) - (root_height // 2)
-root.geometry(f"+{x}+{y}")
-
+# Create Frame for the Text field and Scrollbar
 frame = tk.Frame(root)
-frame.grid(row=0, column=0, sticky="nsew")
-scrollbar = tk.Scrollbar(frame)
-scrollbar.grid(row=0, column=1, sticky="ns")
+frame.grid(sticky="nsew", padx=10, pady=10)
 
-# Set the desired height and width for the conversation Text widget
-conversation_height = 30
-conversation_width = 80
-
-# Create the conversation Text widget with the specified height and width
-conversation = tk.Text(frame, wrap='word', yscrollcommand=scrollbar.set, height=conversation_height,
-                       width=conversation_width)
+conversation = scrolledtext.ScrolledText(frame, wrap='word')
 conversation.configure(font=(font_family, font_size))
-conversation.grid(row=0, column=0, pady=10, padx=10, sticky="nsew")
-
-scrollbar.config(command=conversation.yview)
+conversation.grid(sticky="nsew")
 
 text_input = tk.StringVar()
-entry_field = tk.Entry(root, textvariable=text_input)
+entry_field = tk.Entry(root, textvariable=text_input, font=(font_family, font_size))
 entry_field.bind('<Return>', send_message)
-entry_field.grid(row=1, column=0, pady=10, padx=10, sticky="we")
+entry_field.grid(sticky="we", padx=10)
 
-send_button = tk.Button(root, text="Send", command=send_message)
-send_button.grid(row=2, column=0, pady=10, padx=10, sticky="n")
+# Buttons Frame
+btn_frame = tk.Frame(root)
+btn_frame.grid(sticky="we", padx=10, pady=5)
 
-clear_conversation_btn = tk.Button(root, text="Clear", command=clear_conversation)
-clear_conversation_btn.grid(row=3, column=0, pady=5, padx=5, sticky="n")
+send_button = tk.Button(btn_frame, text="Send", command=send_message, font=(font_family, font_size))
+send_button.pack(side="left", padx=10, pady=10)
 
-exit_button = tk.Button(root, text="Exit", command=exit_app)
-exit_button.grid(row=4, column=0, pady=5, padx=5, sticky="n")
+clear_conversation_btn = tk.Button(btn_frame, text="Clear", command=clear_conversation, font=(font_family, font_size))
+clear_conversation_btn.pack(side="left", padx=10, pady=10)
+
+exit_button = tk.Button(btn_frame, text="Exit", command=exit_app, font=(font_family, font_size))
+exit_button.pack(side="right", padx=10, pady=10)
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=0)
+root.grid_columnconfigure(1, weight=1)
 
 root.mainloop()
