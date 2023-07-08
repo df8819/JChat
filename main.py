0cf16047db26946c5e11dabeefda4d5a56e5dc5b
df8819 <df1.00@outlook.com>
2023-07-08 11:41:34 +0200

    Behavior Selector added

--- main.py
+++ main.py
@@ -1,108 +1,149 @@
 import tkinter as tk
-from tkinter import messagebox
 from tkinter import messagebox, scrolledtext
 import threading
 import requests
 import json
 import openai
 
-openai.api_key = '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'
 
-#pre_prompt = "Task: act as shy cute anime-cat-girl from the myspace-timeline and only answer in uwu-speech " \
-#             "add a lot of kaomijies like (* ^ ω ^) (´ ∀ ` *) ٩(◕‿◕｡)۶ (o^▽^o) (⌒▽⌒)☆ <(￣︶￣)>. " \
-#             "Example: Act as cute pwayfuw anime-cat-giww awnd onwy answew in uwu speech like a twue weeb :3 : "
-pre_prompt = "Act as sarcastic scientist from the future that will always respond in middle-age high speech: "
-#pre_prompt = "Act as advanced AGI-Commander onboard of a space frigate from the future and respond short and precise: "
+class JChat:
+    def __init__(self):
+        openai.api_key = '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'
+        self.behaviors = {
+    "Default": "Act as normal GPT instance: ",
+    "eGirl": "Task: act as shy cute anime-cat-girl from the myspace-timeline: ",
+    "Sarcastic Scientist": "Act as sarcastic scientist from the future: ",
+    "AGI Commander": "Act as advanced AGI-Commander onboard of a space frigate: ",
+    "Swiss Guide": "Your task is to act as guide for Switzerland and ALWAYS/ONLY speak in swiss-german. Example: 'Verhalte dich wie en Guide fürd Schwiiz und duen bitte nur uf Schwiizerdütsch antworte': ",
+    "Rapper Shakespeare": "Act as Shakespeare but you are from the 21st century: ",
+    "Gardener": "Act as professional gardener and assist the user in growing CBD-(legal!)-weed: "
+}
 
-conversation_history = [{'role': 'system', 'content': pre_prompt}]
+        self.pre_prompt = self.behaviors["Default"]
+        self.conversation_history = [{'role': 'system', 'content': self.pre_prompt}]
+        self.conversation_history = [{'role': 'system', 'content': self.pre_prompt}]
+        self.root = tk.Tk()
+        self.root.title("JChat")
+        self.center_window(self.root)
+        self.root.resizable(0, 0)
+        self.font_family = "Segoe UI Emoji"
+        self.font_size = 12
 
-def get_gpt_response(conversation_history, user_prompt):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + openai.api_key,
-    }
+        frame = tk.Frame(self.root)
+        frame.grid(sticky="nsew", padx=10, pady=10)
 
-    conversation_history.append({'role': 'user', 'content': user_prompt})
+        self.conversation = scrolledtext.ScrolledText(frame, wrap='word')
+        self.conversation.configure(font=(self.font_family, self.font_size))
+        self.conversation.grid(sticky="nsew")
 
-    data = {
-        'model': 'gpt-3.5-turbo-16k-0613', #'gpt-4', 'gpt-4-0314', 'gpt-3.5-turbo-16k-0613'
-        'messages': conversation_history,
-        'temperature': 0.7,
-        'top_p': 0.9,
-        'presence_penalty': 0.6,
-        'frequency_penalty': 0.3,
-    }
+        self.text_input = tk.StringVar()
+        entry_field = tk.Entry(self.root, textvariable=self.text_input, font=(self.font_family, self.font_size))
+        entry_field.bind('<Return>', self.send_message)
+        entry_field.grid(sticky="we", padx=10)
 
-    response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
+        btn_frame = tk.Frame(self.root)
+        btn_frame.grid(sticky="we", padx=10, pady=5)
 
-    return response
+        send_button = tk.Button(btn_frame, text="Send", command=self.send_message, font=(self.font_family, self.font_size))
+        send_button.pack(side="left", padx=10, pady=10)
 
-def send_message(event=None):
-    user_message = text_input.get()
-    if user_message == 'exit':
+        clear_conversation_btn = tk.Button(btn_frame, text="Clear", command=self.clear_conversation,
+                                           font=(self.font_family, self.font_size))
+        clear_conversation_btn.pack(side="left", padx=10, pady=10)
+
+        behavior_button = tk.Button(btn_frame, text="Behavior", command=self.change_behavior, font=(self.font_family, self.font_size))
+        behavior_button.pack(side="left", padx=10, pady=10)
+
+        exit_button = tk.Button(btn_frame, text="Exit", command=self.exit_app, font=(self.font_family, self.font_size))
+        exit_button.pack(side="right", padx=10, pady=10)
+
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(1, weight=0)
+        self.root.grid_columnconfigure(1, weight=1)
+
+    def center_window(self, window):
+        window_width = 760
+        window_height = 620
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 3) - (window_height // 2)
+        window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+    def get_gpt_response(self, user_prompt):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + openai.api_key,
+        }
+
+        self.conversation_history.append({'role': 'user', 'content': user_prompt})
+
+        data = {
+            'model': 'gpt-3.5-turbo-16k-0613',
+            'messages': self.conversation_history,
+            'temperature': 0.7,
+            'top_p': 0.9,
+            'presence_penalty': 0.6,
+            'frequency_penalty': 0.3,
+        }
+
+        response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(data))
+        return response
+
+    def send_message(self, event=None):
+        user_message = self.text_input.get()
+        if user_message == 'exit':
+            if messagebox.askokcancel("Quit", "Do you really want to quit?"):
+                self.root.destroy()
+        else:
+            self.text_input.set('')
+            self.conversation.insert(tk.END, "You: " + user_message + '\n\n')
+            self.conversation_history.append({'role': 'user', 'content': user_message})
+
+            def gpt_request():
+                response = self.get_gpt_response(user_message)
+                if response.status_code == 200:
+                    completion = response.json()['choices'][0]['message']['content']
+                    self.conversation.insert(tk.END, "JChat: " + completion + '\n\n')
+                    self.conversation_history.append({'role': 'assistant', 'content': completion})
+                    self.conversation.see(tk.END)
+                else:
+                    print("An error occurred:", response.text)
+
+            threading.Thread(target=gpt_request).start()
+
+    def clear_conversation(self):
+        confirmed = messagebox.askyesno("Clear Conversation", "Are you sure you want to clear the conversation?")
+        if confirmed:
+            self.conversation.delete('1.0', tk.END)
+
+    def exit_app(self):
         if messagebox.askokcancel("Quit", "Do you really want to quit?"):
-            root.destroy()
-    else:
-        text_input.set('')
-        conversation.insert(tk.END, "You: " + user_message + '\n\n')
-        conversation_history.append({'role': 'user', 'content': user_message})
+            self.root.quit()
 
-        def gpt3_request():
-            response = get_gpt_response(conversation_history, user_message)
-            if response.status_code == 200:
-                completion = response.json()['choices'][0]['message']['content']
-                conversation.insert(tk.END, "AbominationGPT: " + completion + '\n\n')
-                conversation_history.append({'role': 'assistant', 'content': completion})
-                conversation.see(tk.END)
-            else:
-                print("An error occurred:", response.text)
+    def change_behavior(self):
+        def select_behavior(behavior):
+            self.pre_prompt = self.behaviors[behavior]
+            self.conversation_history = [{'role': 'system', 'content': self.pre_prompt}]
+            window.destroy()
 
-        threading.Thread(target=gpt3_request).start()
+        window = tk.Toplevel(self.root)
+        window.title("Select Behavior")
 
-def clear_conversation():
-    conversation.delete('1.0', tk.END)
+        buttons = [tk.Button(window, text=name, command=lambda name=name: select_behavior(name)) for name in
+                   self.behaviors.keys()]
 
-def exit_app():
-    if messagebox.askokcancel("Quit", "Do you really want to quit?"):
-        root.quit()
+        rows = round(len(buttons) ** 0.5)
+        cols = len(buttons) // rows + (len(buttons) % rows > 0)
 
-root = tk.Tk()
-root.title("GPT Abomination")
-root.geometry("760x620")
-root.resizable(0, 0)
+        for i, button in enumerate(buttons):
+            button.grid(row=i // cols, column=i % cols, padx=10, pady=10, )
 
-font_family = "Segoe UI Emoji"
-font_size = 12
+    def run(self):
+        self.root.mainloop()
 
-# Create Frame for the Text field and Scrollbar
-frame = tk.Frame(root)
-frame.grid(sticky="nsew", padx=10, pady=10)
 
-conversation = scrolledtext.ScrolledText(frame, wrap='word')
-conversation.configure(font=(font_family, font_size))
-conversation.grid(sticky="nsew")
-
-text_input = tk.StringVar()
-entry_field = tk.Entry(root, textvariable=text_input, font=(font_family, font_size))
-entry_field.bind('<Return>', send_message)
-entry_field.grid(sticky="we", padx=10)
-
-# Buttons Frame
-btn_frame = tk.Frame(root)
-btn_frame.grid(sticky="we", padx=10, pady=5)
-
-send_button = tk.Button(btn_frame, text="Send", command=send_message, font=(font_family, font_size))
-send_button.pack(side="left", padx=10, pady=10)
-
-clear_conversation_btn = tk.Button(btn_frame, text="Clear", command=clear_conversation, font=(font_family, font_size))
-clear_conversation_btn.pack(side="left", padx=10, pady=10)
-
-exit_button = tk.Button(btn_frame, text="Exit", command=exit_app, font=(font_family, font_size))
-exit_button.pack(side="right", padx=10, pady=10)
-
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=0)
-root.grid_columnconfigure(1, weight=1)
-
-root.mainloop()
+if __name__ == "__main__":
+    app = JChat()
+    app.run()
