4219deeaf408c79739e6756df813c766ebc1e599
df8819 <df1.00@outlook.com>
2023-07-10 02:02:48 +0200

    Run.bat includes rename of temp.apikey.json

--- Run.bat
+++ Run.bat
@@ -1,3 +1,11 @@
 @echo off
 cd /d "%~dp0"
+
+REM Function to rename the file
+:RENAME_FILE
+IF EXIST temp.apikey.json (
+  ren temp.apikey.json apikey.json
+)
+
+REM Run the Python script
 python main.py
